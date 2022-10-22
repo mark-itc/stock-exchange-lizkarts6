e961962ba6a049f35cc9cc45c17a87f5e6e79ef2
Liza <lizkarts6@gmail.com>
2022-10-23 02:05:06 +0300

    milestone 2 done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchButton = document
   .getElementById("submitButton")
   .addEventListener("click", searchData);
-const userInput = document.getElementById("userInput").value;
+const userInput = document.getElementById("userSearchInput");
 const searchedList = document.getElementById("searchedList");
 const loadingSpinner = document.getElementById("spinner");
 
@@ -9,11 +9,20 @@ function searchData(e) {
   e.preventDefault();
   getSearchData(
     "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=" +
-      userInput +
+      userInput.value +
       "&limit=10&exchange=NASDAQ"
   );
 }
+// let urlParams = new URLSearchParams(window.location.search);
+// const symbol = urlParams.get("symbol");
 
+// async function getImgAndPrice (){
+//   const response = await fetch (`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`)
+//   const result = await response.json()
+//   console.log(result);
+// }
+
+// getImgAndPrice()
 async function getSearchData(url) {
   loadingSpinner.classList.remove("d-none");
   searchedList.innerHTML = "";
@@ -21,21 +30,21 @@ async function getSearchData(url) {
     const response = await fetch(url);
     const result = await response.json();
     loadingSpinner.classList.add("d-none");
-    result.forEach((item) => {
-      let name = item.name;
-      let symbol = item.symbol;
+    for (let i = 0; i < result.length; i++) {
+      let symbol = result[i].symbol;
+      let name = result[i].name;
       let newLi = document.createElement("li");
       newLi.classList.add("list-of-results");
       newLi.innerHTML =
-        '<a target="_blank" href=/company.html?symbol=' +
+        ' <a target="_blank" href=/company.html?symbol=' +
         symbol +
         ">" +
         name +
         "(" +
         symbol +
-        ")</a>";
+        ")</a> ";
       searchedList.appendChild(newLi);
-    });
+    }
   } catch (err) {
     throw new Error("error", err);
   }
